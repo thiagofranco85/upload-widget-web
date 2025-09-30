a4c5ed5917e3fdc3f6cbf7fe1d9607ab84d81aeb
Thiago Franco <thiago.boo@gmail.com>
2025-09-30 14:38:18 -0300

    upload initial files

---
+++ src/components/upload-widget-upload-list.tsx
@@ -0,0 +1,30 @@
+import { useUploads } from '../store/uploads';
+import { UploadWidgetUploadItem } from './upload-widget-upload-item';
+
+
+export function UploadWidgetUploadList(){
+
+    const {uploads} = useUploads();
+
+    const isUploadListEmpty = uploads.size === 0;
+
+    return (
+        <div className="px-3 flex flex-col gap-3">
+            <span className="text-xs font-medium">
+                Uploaded Files{''}<span>(2)
+                </span>
+            </span>
+
+         
+            {isUploadListEmpty ? (
+                <span className='text-xs text-zinc-400'>No uploads added</span>
+            ) : (
+                <div className="flex flex-col gap-2">
+                    { Array.from(uploads.entries()).map(([uploadId, upload])=>{
+                        return <UploadWidgetUploadItem key={uploadId} upload={upload} />
+                    })}
+                </div>
+            )}
+        </div>
+    )
+}
