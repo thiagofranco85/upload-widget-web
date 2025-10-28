5d4b6ffa5f3d1989662f82dab9c0e61f847359cd
Thiago Franco <thiago.boo@gmail.com>
2025-10-28 14:09:02 -0300

    Adiciona exibição de progresso de upload e atualiza o tamanho original do arquivo

--- src/store/uploads.ts
+++ src/store/uploads.ts
@@ -1,3 +1,4 @@
+import { CanceledError } from "axios";
 import { enableMapSet } from "immer";
 import { create } from "zustand";
 import { immer } from "zustand/middleware/immer";
@@ -8,6 +9,8 @@ export type Upload = {
   file: File;
   abortController: AbortController;
   status: "progress" | "success" | "error" | "canceled";
+  originalSizeInBytes: number;
+  uploadSizeInBytes: number;
 };
 
 type UploadState = {
@@ -29,7 +32,17 @@ export const useUploads = create<UploadState, [["zustand/immer", never]]>(
 
       try {
         await uploadFileToStorage(
-          { file: upload.file },
+          {
+            file: upload.file,
+            onProgress(sizeInBytes) {
+              set((state) => {
+                state.uploads.set(uploadId, {
+                  ...upload,
+                  uploadSizeInBytes: sizeInBytes,
+                });
+              });
+            },
+          },
           { signal: upload.abortController.signal }
         );
 
@@ -39,7 +52,18 @@ export const useUploads = create<UploadState, [["zustand/immer", never]]>(
             status: "success",
           });
         });
-      } catch (error) {
+      } catch (err) {
+        if (err instanceof CanceledError) {
+          set((state) => {
+            state.uploads.set(uploadId, {
+              ...upload,
+              status: "canceled",
+            });
+          });
+
+          return;
+        }
+
         set((state) => {
           state.uploads.set(uploadId, {
             ...upload,
@@ -76,6 +100,8 @@ export const useUploads = create<UploadState, [["zustand/immer", never]]>(
           file,
           abortController,
           status: "progress",
+          originalSizeInBytes: file.size,
+          uploadSizeInBytes: 0,
         };
 
         set((state) => {
